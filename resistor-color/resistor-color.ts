cd7d1e4248a0f1c42e86cf33040b642cd4c5869c
Matt Kantor <the.matt.kantor@gmail.com>
2025-08-08 07:02:05 -0400

    Implement resistor-color.

--- resistor-color/resistor-color.ts
+++ resistor-color/resistor-color.ts
@@ -1,5 +1,17 @@
-export const colorCode = () => {
-  throw new Error('Remove this line and implement the function')
+const resistanceByColor = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
 }
+type Color = keyof typeof resistanceByColor
 
-export const COLORS = undefined
+export const colorCode = (color: Color): number => resistanceByColor[color]
+
+export const COLORS = Object.keys(resistanceByColor)
