f4d9c2afc5cfa522da6204104850f0224380e044
Matt Kantor <the.matt.kantor@gmail.com>
2025-08-12 07:41:39 -0400

    Implement clock.

--- clock/clock.ts
+++ clock/clock.ts
@@ -1,21 +1,43 @@
 export class Clock {
-  constructor(hour: unknown, minute?: unknown) {
-    throw new Error('Remove this line and implement the function')
+  /** This is always less than `minutesPerDay`. */
+  #minuteWithinDay = 0n
+
+  constructor(hours: number, minutes: number = 0) {
+    this.plus(hours * Number(minutesPerHour) + minutes)
   }
 
-  public toString(): unknown {
-    throw new Error('Remove this line and implement the function')
+  public toString(): string {
+    const hourWithinDay = this.#minuteWithinDay / minutesPerHour
+    const minuteWithinHour =
+      this.#minuteWithinDay - hourWithinDay * minutesPerHour
+
+    const formattedHour = String(hourWithinDay).padStart(2, '0')
+    const formattedMinute = String(minuteWithinHour).padStart(2, '0')
+
+    return `${formattedHour}:${formattedMinute}`
   }
 
-  public plus(minutes: unknown): Clock {
-    throw new Error('Remove this line and implement the function')
+  public plus(minutes: number): Clock {
+    const minutesToAdd =
+      minutes < 0
+        ? (BigInt(minutes) % minutesPerDay) + minutesPerDay
+        : BigInt(minutes)
+
+    this.#minuteWithinDay =
+      (this.#minuteWithinDay + minutesToAdd) % minutesPerDay
+
+    return this
   }
 
-  public minus(minutes: unknown): Clock {
-    throw new Error('Remove this line and implement the function')
+  public minus(minutes: number): Clock {
+    return this.plus(-minutes)
   }
 
-  public equals(other: unknown): unknown {
-    throw new Error('Remove this line and implement the function')
+  public equals(other: Clock): boolean {
+    return this.#minuteWithinDay === other.#minuteWithinDay
   }
 }
+
+const hoursPerDay = 24n
+const minutesPerHour = 60n
+const minutesPerDay = hoursPerDay * minutesPerHour
