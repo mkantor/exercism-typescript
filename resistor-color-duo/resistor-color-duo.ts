08e4bd48af91ccecd68a384a00d22511c73c79c5
Matt Kantor <the.matt.kantor@gmail.com>
2025-08-19 07:18:29 -0400

    Implement resistor-color-duo.

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,21 @@
-export function decodedValue() {
-  throw new Error('Remove this line and implement the function')
+const resistanceByColor = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+}
+type Color = keyof typeof resistanceByColor
+
+export function decodedValue(
+  colors: readonly [Color, Color, ...Color[]],
+): number {
+  const firstDigit = resistanceByColor[colors[0]]
+  const secondDigit = resistanceByColor[colors[1]]
+  return firstDigit * 10 + secondDigit
 }
