38a4d8bc5c761895b5dbc6827685663a09fe7e0e
Matt Kantor <the.matt.kantor@gmail.com>
2025-08-22 09:20:18 -0400

    Implement rational-numbers.

--- rational-numbers/rational-numbers.ts
+++ rational-numbers/rational-numbers.ts
@@ -1,37 +1,70 @@
+const greatestCommonDivisor = (a: number, b: number): number =>
+  b === 0 ? Math.abs(a) : greatestCommonDivisor(b, a % b)
+
 export class Rational {
-  constructor() {
-    throw new Error('Remove this line and implement the function')
+  readonly numerator
+  readonly denominator
+
+  constructor(numerator: number, denominator: number) {
+    this.numerator = numerator
+    this.denominator = denominator
   }
 
-  add() {
-    throw new Error('Remove this line and implement the function')
+  add(operand: Rational): Rational {
+    return new Rational(
+      this.numerator * operand.denominator +
+        operand.numerator * this.denominator,
+      this.denominator * operand.denominator,
+    ).reduce()
   }
 
-  sub() {
-    throw new Error('Remove this line and implement the function')
+  sub(operand: Rational): Rational {
+    return this.add(new Rational(-operand.numerator, operand.denominator))
   }
 
-  mul() {
-    throw new Error('Remove this line and implement the function')
+  mul(operand: Rational): Rational {
+    return new Rational(
+      this.numerator * operand.numerator,
+      this.denominator * operand.denominator,
+    ).reduce()
   }
 
-  div() {
-    throw new Error('Remove this line and implement the function')
+  div(operand: Rational): Rational {
+    return this.mul(new Rational(operand.denominator, operand.numerator))
   }
 
-  abs() {
-    throw new Error('Remove this line and implement the function')
+  abs(): Rational {
+    const reduced = this.reduce()
+    return new Rational(Math.abs(reduced.numerator), reduced.denominator)
   }
 
-  exprational() {
-    throw new Error('Remove this line and implement the function')
+  exprational(exponent: number): Rational {
+    if (exponent >= 0) {
+      return new Rational(
+        this.numerator ** exponent,
+        this.denominator ** exponent,
+      ).reduce()
+    } else {
+      const positiveExponent = -exponent
+      return new Rational(
+        this.denominator ** positiveExponent,
+        this.numerator ** positiveExponent,
+      ).reduce()
+    }
   }
 
-  expreal() {
-    throw new Error('Remove this line and implement the function')
+  expreal(base: number): number {
+    return base ** (this.numerator / this.denominator)
   }
 
-  reduce() {
-    throw new Error('Remove this line and implement the function')
+  reduce(): Rational {
+    const gcd = greatestCommonDivisor(this.numerator, this.denominator)
+    const newNumerator = this.numerator / gcd
+    const newDenominator = this.denominator / gcd
+    if (newDenominator < 0) {
+      return new Rational(-newNumerator, -newDenominator)
+    } else {
+      return new Rational(newNumerator, newDenominator)
+    }
   }
 }
