bdfd27a871321cf450c52c61f0d3e001fc80a183
Matt Kantor <the.matt.kantor@gmail.com>
2025-08-22 12:35:34 -0400

    Implement space-age.

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,3 +1,16 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+const secondsPerYearByPlanet = {
+  mercury: 7600544,
+  venus: 19414149,
+  earth: 31557600,
+  mars: 59354033,
+  jupiter: 374355659,
+  saturn: 929292363,
+  uranus: 2651370019,
+  neptune: 5200418560,
+}
+
+type PlanetName = keyof typeof secondsPerYearByPlanet
+
+export function age(planet: PlanetName, seconds: number): number {
+  return Math.round((seconds / secondsPerYearByPlanet[planet]) * 100) / 100
 }
