9e359aa9ea52b81f8407f97cfc375c956c625f9f
Matt Kantor <the.matt.kantor@gmail.com>
2025-08-27 05:11:59 -0400

    Implement rna-transcription.

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,3 +1,22 @@
-export function toRna() {
-  throw new Error('Remove this line and implement the function')
+export function toRna(dna: string): string {
+  let rna = ''
+  for (const dnaNucleotide of dna) {
+    switch (dnaNucleotide) {
+      case 'G':
+        rna += 'C'
+        break
+      case 'C':
+        rna += 'G'
+        break
+      case 'T':
+        rna += 'A'
+        break
+      case 'A':
+        rna += 'U'
+        break
+      default:
+        throw new Error('Invalid input DNA.')
+    }
+  }
+  return rna
 }
