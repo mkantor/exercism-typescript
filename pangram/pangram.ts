d3beb5ead4374462ca0bae96cf1c2d835ce777cd
Matt Kantor <the.matt.kantor@gmail.com>
2025-08-28 13:02:46 -0400

    Implement pangram.

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -1,3 +1,13 @@
-export function isPangram() {
-  throw new Error('Remove this line and implement the function')
+export function isPangram(input: string): boolean {
+  const uniqueLetters = new Set<string>()
+  for (const character of input) {
+    const lowercaseCharacter = character.toLowerCase()
+    if (/[a-z]/.test(lowercaseCharacter)) {
+      uniqueLetters.add(lowercaseCharacter)
+    }
+    if (uniqueLetters.size === 26) {
+      return true
+    }
+  }
+  return false
 }
